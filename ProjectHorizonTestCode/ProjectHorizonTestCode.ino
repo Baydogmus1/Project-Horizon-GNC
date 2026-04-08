1c73c0a62d4002ed4597dc80e54e5df5f83c6d1d
Baydogmus1 <be930695@ucf.edu>
2026-04-08 17:51:38 -0400

    Update ProjectHorizonTestCode.ino

--- ProjectHorizonTestCode/ProjectHorizonTestCode.ino
+++ ProjectHorizonTestCode/ProjectHorizonTestCode.ino
@@ -140,8 +140,8 @@ void LSM6DSO32Setup() {
   if (!dso32.begin_I2C()) while (1); 
   dso32.setAccelRange(LSM6DSO32_ACCEL_RANGE_16_G);
   dso32.setGyroRange(LSM6DS_GYRO_RANGE_2000_DPS);
-  dso32.setAccelDataRate(LSM6DS_RATE_833_HZ);
-  dso32.setGyroDataRate(LSM6DS_RATE_833_HZ);
+  dso32.setAccelDataRate(LSM6DS_RATE_6_66K_HZ);
+  dso32.setGyroDataRate(LLSM6DS_RATE_6_66K_HZ);
 }
 
 void calibrategyro() {
